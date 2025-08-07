250c43c4853ae979b266664eca66d6d0286d386b
ggopichandu <gummagopichand@gmail.com>
2025-08-07 15:49:09 +0530

    bastion

---
+++ 04-db/main.tf
@@ -0,0 +1,66 @@
+# module "db" {
+#   source = "terraform-aws-modules/rds/aws"
+
+#   identifier = "/${var.project_name}/${var.environment}" #expense-dev
+
+#   engine            = "mysql"
+#   engine_version    = "8.0"
+#   instance_class    = "db.t3.micro"
+#   allocated_storage = 5
+
+#   db_name  = "transactions" #default schema for expense project
+#   username = "root"
+#   port     = "3306"
+
+#   vpc_security_group_ids = [data.aws_ssm_parameter.db_sg_id.value]
+
+
+
+#   # DB subnet group
+#   db_subnet_group_name = 
+
+#   # DB parameter group
+#   family = "mysql5.7"
+
+#   # DB option group
+#   major_engine_version = "5.7"
+
+#   # Database Deletion Protection
+#   deletion_protection = true
+
+#   parameters = [
+#     {
+#       name  = "character_set_client"
+#       value = "utf8mb4"
+#     },
+#     {
+#       name  = "character_set_server"
+#       value = "utf8mb4"
+#     }
+#   ]
+
+#   options = [
+#     {
+#       option_name = "MARIADB_AUDIT_PLUGIN"
+
+#       option_settings = [
+#         {
+#           name  = "SERVER_AUDIT_EVENTS"
+#           value = "CONNECT"
+#         },
+#         {
+#           name  = "SERVER_AUDIT_FILE_ROTATIONS"
+#           value = "37"
+#         },
+#       ]
+#     },
+#   ]
+
+  
+#   tags = {
+#     Owner       = "user"
+#     Environment = "dev"
+#   }
+# }
+
+

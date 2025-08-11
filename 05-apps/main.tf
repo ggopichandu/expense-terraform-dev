ec46ed0815766b05080ad2836c27f73f7d52d212
ggopichandu <gummagopichand@gmail.com>
2025-08-12 02:24:22 +0530

    apps

--- 05-apps/main.tf
+++ 05-apps/main.tf
@@ -1,37 +1,65 @@
-module "backend" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
+# module "backend" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "${var.project_name}-${var.environment}-backend"
+#   name = "${var.project_name}-${var.environment}-backend"
 
-  instance_type = "t3.micro"
+#   instance_type = "t3.micro"
+#   vpc_security_group_ids = [data.aws_ssm_parameter.backend_sg_id.value]
+#   # Convert StringList to list and get first element
+#   subnet_id = local.private_subnet_id
+#   ami = data.aws_ami.ami_info.id
+
+#   tags = merge(
+#         var.common_tags,
+#     {
+#         Name = "${var.project_name}-${var.environment}-backend"
+#     }
+#     )
+#   }
+
+#   module "frontend" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+
+#   name = "${var.project_name}-${var.environment}-frontend"
+
+#   instance_type = "t3.micro"
+#   vpc_security_group_ids = [data.aws_ssm_parameter.frontend_sg_id.value]
+#   # Convert StringList to list and get first element
+#   subnet_id = local.public_subnet_id
+#   ami = data.aws_ami.ami_info.id
+
+#   tags = merge(
+#         var.common_tags,
+#     {
+#         Name = "${var.project_name}-${var.environment}-frontend"
+#     }
+#     )
+#   }
+
+
+  ##new code
+resource "aws_instance" "backend" {
+  ami                    = data.aws_ami.ami_info.id
   vpc_security_group_ids = [data.aws_ssm_parameter.backend_sg_id.value]
-  # Convert StringList to list and get first element
-  subnet_id = local.private_subnet_id
-  ami = data.aws_ami.ami_info.id
-
+  instance_type          = "t3.micro"
+  subnet_id   = local.private_subnet_id
   tags = merge(
-        var.common_tags,
+    var.common_tags,
     {
         Name = "${var.project_name}-${var.environment}-backend"
     }
-    )
-  }
+  )
+}
 
-  module "frontend" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-
-  name = "${var.project_name}-${var.environment}-frontend"
-
-  instance_type = "t3.micro"
+resource "aws_instance" "frontend" {
+  ami                    = data.aws_ami.ami_info.id
   vpc_security_group_ids = [data.aws_ssm_parameter.frontend_sg_id.value]
-  # Convert StringList to list and get first element
-  subnet_id = local.public_subnet_id
-  ami = data.aws_ami.ami_info.id
-
+  instance_type          = "t3.micro"
+  subnet_id   = local.public_subnet_id
   tags = merge(
-        var.common_tags,
+    var.common_tags,
     {
         Name = "${var.project_name}-${var.environment}-frontend"
     }
-    )
-  }
+  )
+}

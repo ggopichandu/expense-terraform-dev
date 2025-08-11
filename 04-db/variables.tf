3ca79c903ffa4b8b976fb634bfbb6f348507b404
ggopichandu <gummagopichand@gmail.com>
2025-08-11 18:42:42 +0530

    vpc

--- 04-db/variables.tf
+++ 04-db/variables.tf
@@ -20,3 +20,12 @@ variable "db_sg_discription" {
     default = "SG for DB MySQL Instances"
   
 }
+
+
+variable "zone_id" {
+    default = "Z05089881SMV5BSCETO2M"
+}
+
+variable "domain_name" {
+    default = "gopichand.online"
+}
